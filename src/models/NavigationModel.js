01a4a5476a7e968da0d3ff1fb77211f202a4cd53
newmediapilot <marcin@newmediapilot.com>
2020-09-14 19:10:21 -0400

    add model

---
+++ src/models/NavigationModel.js
@@ -0,0 +1,12 @@
+const NavigationModel = [
+    {
+        displayValue: 'tab1',
+        active: false
+    },
+    {
+        displayValue: 'tab2',
+        active: false
+    }
+];
+
+export default NavigationModel;
