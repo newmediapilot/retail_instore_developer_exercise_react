736b67ab7a95ab6bd473a1794da8df10a397c331
newmediapilot <marcin@newmediapilot.com>
2020-09-14 21:24:28 -0400

    extract variables

--- src/models/NavigationModel.js
+++ src/models/NavigationModel.js
@@ -1,14 +1,39 @@
 const NavigationModel = [
     {
-        label: 'tab1',
+        label: 'Cupertino',
         active: false,
-        route:'#tab1'
+        route: '#Cupertino'
     },
     {
-        label: 'tab2',
+        label: 'New York City',
         active: false,
-        route:'#tab2'
-    }
+        route: '#NewYorkCity'
+    },
+    {
+        label: 'London',
+        active: false,
+        route: '#London'
+    },
+    {
+        label: 'Amsterdam',
+        active: false,
+        route: '#Amsterdam'
+    },
+    {
+        label: 'Tokyo',
+        active: false,
+        route: '#Tokyo'
+    },
+    {
+        label: 'Hong Kong',
+        active: false,
+        route: '#Hong Kong'
+    },
+    {
+        label: 'Sydney',
+        active: false,
+        route: '#Sydney'
+    },
 ];
 
 export default NavigationModel;
