532f8978141f6224eeddcfe843ad62fcb84c46a7
newmediapilot <marcin@newmediapilot.com>
2020-09-15 02:51:05 -0400

    adding build files for gh pages

---
+++ build/precache-manifest.2195def32af251067eeba9c652e535a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e41f38123948b63934d0056ca84875",
+    "url": "/retail_instore_developer_exercise_react/build/index.html"
+  },
+  {
+    "revision": "d8449875d157b303712b",
+    "url": "/retail_instore_developer_exercise_react/build/static/css/main.5c193a04.chunk.css"
+  },
+  {
+    "revision": "ba593929e7815cffc223",
+    "url": "/retail_instore_developer_exercise_react/build/static/js/2.b0c9d9be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/retail_instore_developer_exercise_react/build/static/js/2.b0c9d9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8449875d157b303712b",
+    "url": "/retail_instore_developer_exercise_react/build/static/js/main.836c15b7.chunk.js"
+  },
+  {
+    "revision": "5a430593dd7d631255f3",
+    "url": "/retail_instore_developer_exercise_react/build/static/js/runtime-main.e9cc2788.js"
+  }
+]);
